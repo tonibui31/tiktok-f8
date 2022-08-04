6abd54ee4e51649fec963aa4d901fa6c36d9125b
Toni Bui <tom.khoibui31071999@gmail.com>
2022-08-04 12:06:32 +0700

    UI phần Header #1

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,14 +1,35 @@
 import classNames from 'classnames/bind';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons';
+
 import styles from './Header.module.scss';
+import images from '~/assets/images';
 
 const cx = classNames.bind(styles);
 
 function Header() {
-    return (
-        <header className={cx('wrapper')}>
-            <div className={cx('inner')}></div>
-        </header>
-    );
+  return (
+    <header className={cx('wrapper')}>
+      <div className={cx('inner')}>
+        <div className={cx('logo')}>
+          <img src={images.logo} alt="Tiktok" />
+        </div>
+        <div className={cx('search')}>
+          <input placeholder="Search accounts and videos" spellCheck={false} />
+          <button className={cx('clear')}>
+            <FontAwesomeIcon icon={faCircleXmark} />
+          </button>
+          <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+          <button className={cx('searchButton')}>
+          <FontAwesomeIcon icon={faMagnifyingGlass} />
+          </button>
+        </div>
+        <div className={cx('actions')}>
+            <h4>Placeholder</h4>
+        </div>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
